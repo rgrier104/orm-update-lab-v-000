e5cdf1c7134c122ce897bffb1d1e0856356524af
Rachel Grier <rgrier104@gmail.com>
2019-01-12 23:32:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,7 @@ def self.find_by_name(name)
   end.first
 end
 
-  def self.create(name:,grade:)
+  def self.create(name:, grade:)
     student = Student.new(name,grade)
     student.save
     student
